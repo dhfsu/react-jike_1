856fe31412b785fd3d5c6ef3aacde6e9786fb859
dhfsu <3227978070@qq.com>
2026-03-13 16:15:54 +0800

    退出登录实现

--- src/store/modules/user.jsx
+++ src/store/modules/user.jsx
@@ -1,21 +1,30 @@
 import { request } from "@/utils/request";
 import { createSlice } from "@reduxjs/toolkit";
-import { setToken as _setToken,getToken as _getToken } from "@/utils/main";
+import { setToken as _setToken,getToken as _getToken, removeToken } from "@/utils/main";
 
 const userStore = createSlice({
     name: "user",
     initialState: {
-        token: _getToken() || ""
+        token: _getToken() || "",
+        userInfo:{}
     },
     reducers: {
         setToken(state, action) {
             state.token = action.payload //这行代码是存到了redux里
             _setToken(action.payload) //这行是存到了本地
+        },
+        setUserInfo(state,action){
+            state.userInfo=action.payload
+        },
+        clearUserInfo(state){
+            state.token = ''
+            state.userInfo = {}
+            removeToken()
         }
     }
 })
 
-const { setToken } = userStore.actions
+const { setToken,setUserInfo,clearUserInfo } = userStore.actions
 const userReducer = userStore.reducer
 
 //异步方法 完成登陆获取token
@@ -25,5 +34,13 @@ const fetchLogin = (loginForm) => {
         dispatch(setToken(res.data.token))
     }
 }
-export { setToken, fetchLogin }
+
+const fetchUserInfo = () => {
+    return async (dispatch) => {
+        const res = await request.get("/user/profile")
+        dispatch(setUserInfo(res.data))
+    }
+}
+
+export { setToken, fetchLogin,fetchUserInfo,clearUserInfo }
 export default userReducer
